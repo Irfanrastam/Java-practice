8e3c2169620bb17556713cf81fc6e8502dc52999
Irex_Novax <130748491+Irfanrastam@users.noreply.github.com>
2026-09-04 22:48:42 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -4,9 +4,9 @@ class Calculator
 	{	
 		int a = 10;
 		int b = 20;
-		System.out.println("Addition:"+(a + b));
-		System.out.println("Subtraction:"+(a - b));
-		System.out.println("Multiplication:"+(a * b));
-		System.out.println("Division:"+(a / b));
+		System.out.println("Addition:"+ (a + b));
+		System.out.println("Subtraction:"+ (a - b));
+		System.out.println("Multiplication:"+ (a * b));
+		System.out.println("Division:"+ (a / b));
 	}
 }
